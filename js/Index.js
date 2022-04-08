bdd2e95bea69e723701fb56d1507b4184054af4b
koent <koent@LAPTOP>
2022-04-08 22:00:38 +0200

    added desktop version

--- js/Index.js
+++ js/Index.js
@@ -3,7 +3,7 @@ const socialDiv = document.querySelector(".social-icons");
 const shareDiv = document.querySelector(".share-div");
 
 shareBtn.addEventListener("click", function () {
-  socialDiv.classList.add("active");
+  socialDiv.classList.toggle("active");
 });
 
 shareDiv.addEventListener("click", function () {
